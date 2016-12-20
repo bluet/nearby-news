71c1ffd5184f7d8c47b65bd5d3de38003b85197e
BlueT - Matthew - 練喆明 <BlueT@BlueT.org>
2016-12-20 11:33:50 +0800

    load rethinkdbdash

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var r = require('rethinkdbdash')();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
