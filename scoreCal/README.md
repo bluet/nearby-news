0b9d8245707ded4a38f16dc4f81921ad165cbc36
Xiao Sheng <david2881234@gmail.com>
2017-01-02 14:38:13 +0800

    score calculation

---
+++ scoreCal/README.md
@@ -0,0 +1,4 @@
+## 權重計算 ##
+- 先生成一個middle object，它包含每個keyword的timestamp
+- 再透過midlle object，把每個keyword的score算出來
+- 演算法是原本討論好的 1/timeDiff
---
+++ scoreCal/scoreCal.js
@@ -0,0 +1,31 @@
+//read the JSON file
+var fs = require('fs');
+var rawObj = JSON.parse(fs.readFileSync('testfile.json', 'utf8'));
+var currentTime = Math.floor(Date.now() / 1000);
+var middleObj = [];
+var targetObj = [];
+
+// score calculation
+function calculateScore(timeDiff) {
+  return 1 / (timeDiff / 86400 / 30);
+}
+
+//transfer the raw object to the middle object
+for (var i = 0; i < rawObj.length; i++) {
+  for(var j = 0; j < rawObj[i].keywords.length; j++) {
+    middleObj.push({"keywords":rawObj[i].keywords[j], "timestamp":rawObj[i].timestamp});
+  }
+}
+
+//transfer the miidle object to the target object
+for (var i = 0; i < middleObj.length; i++) {
+  var timeDiff = Math.floor(currentTime - middleObj[i].timestamp);
+  var index = targetObj.map(function(o) { return o.name; }).indexOf(middleObj[i].keywords); // If keywords were not found in target object, push it. Otherwise, re-caculate the score of the keyword
+  if (index === -1) {
+    targetObj.push({"name":middleObj[i].keywords, "score": calculateScore(timeDiff)});
+  }
+  else {
+    targetObj[index].score += calculateScore(timeDiff);
+  }
+}
+console.log(targetObj);
