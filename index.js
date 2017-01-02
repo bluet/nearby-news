04b8448347582aede0f9d4c6b017794f8858a2dd
BlueT - Matthew Lien - 練喆明 <BlueT@BlueT.org>
2017-01-02 18:54:03 +0800

    fixed: bind on all IP

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   app.use(middleware.swaggerUi());
 
   // Start the server
-  http.createServer(app).listen(serverPort, function () {
+  http.createServer(app).listen(serverPort, "0.0.0.0", function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
   });
